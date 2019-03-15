f3f94ade32ba2d1f5927dfe09681d27d90440cc2
Lyneca <atraguliauniliantauthr@gmail.com>
2019-03-15 14:35:05 +1100

    Add the CDN URL to the right file this time

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ let sketches = firestore.collection("sketches")
 
 const HTML_HEAD = `
 <head>
-    <script src="p5.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/p5.js/0.7.3/p5.min.js"></script>
     <style>
         body {
             margin: 0;
